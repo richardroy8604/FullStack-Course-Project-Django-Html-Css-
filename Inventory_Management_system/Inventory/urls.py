efa245e30270be2d016ea8551cd81641c9fad044
richardroy8604 <richardroy8604@gmail.com>
2026-07-31 18:33:58 +0530

    Update Inventory Management System: add item editing, views, migrations, and template updates

--- Inventory_Management_system/Inventory/urls.py
+++ Inventory_Management_system/Inventory/urls.py
@@ -3,4 +3,8 @@ from Inventory import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_item, name='add_item'),
+    path('update-stock/<int:item_id>/<str:action>/', views.update_stock, name='update_stock'),
+    path('edit/', views.edit_item, name='edit_item'),
+    path('update-product/<int:item_id>/', views.update_product, name='update_product'),
+    path('delete-item/<int:item_id>/', views.delete_item, name='delete_item'),
 ]
--- Inventory_Management_system/Inventory/views.py
+++ Inventory_Management_system/Inventory/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from Inventory.models import Category, Product
 
 # Create your views here.
  
@@ -12,7 +13,43 @@ def add_item(request):
         quantity = request.POST['quantity']
         price = request.POST['price']
         category = request.POST['category']
-        # Save the data to the database or perform other actions
-    return render(request, 'add_item.html')
+
+        Product.objects.create(name=name, quantity=quantity, price=price, category_id=category)
+        return redirect('add_item')  
+    
+    categories= Category.objects.all()
+    items = Product.objects.all()
+    return render(request, 'add_item.html', {'categories':categories, 'items':items})
+
+def update_stock(request, item_id, action):
+    product = Product.objects.get(id=item_id)
+    if action == 'add':
+        product.quantity += 1                               
+    elif action == 'remove':
+        product.quantity -= 1
+    product.save()
+    return redirect('add_item')    
+
+def edit_item(request):
+    items = Product.objects.all()
+    return render(request, 'edit_item.html', {'items':items})
+    
+def update_product(request, item_id):
+    product = Product.objects.get(id=item_id)
+    if request.method == 'POST':
+        product.name = request.POST['name']
+        product.quantity = request.POST['quantity']
+        product.price = request.POST['price']
+        product.category_id = request.POST['category']
+        product.save()
+        return redirect('edit_item')
+        
+    categories = Category.objects.all()
+    return render(request, 'update_item.html', {'product': product, 'categories': categories})
+
+def delete_item(request, item_id):
+    product = Product.objects.get(id=item_id)
+    product.delete()
+    return redirect('edit_item')
 
 
