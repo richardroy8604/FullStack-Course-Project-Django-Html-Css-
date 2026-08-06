3b9776bf5df1bf0a9d48176ad25403d0eb3602ab
richardroy8604 <richardroy8604@gmail.com>
2026-08-06 15:08:06 +0530

    Add Login_auth_implimentation project with login authentication templates and views

---
+++ Login_auth_implimentation/accounts/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path,include
+from accounts import views
+
+urlpatterns = [
+    path('', views.login_view, name='login'),
+    path('home/', views.home, name='home'),
+    path('logout/', views.logout_view, name='logout')
+]
---
+++ Login_auth_implimentation/accounts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to a success page.
+        return render(request, 'login.html', {'error': 'Invalid username or password.'})
+
+
+    return render(request, 'login.html')
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Redirect to the login page after logout
